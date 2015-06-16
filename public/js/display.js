bcf1a4dd011706598539c0f747664e513f8a2212
JonasStattin <jonas.i.stattin@gmail.com>
2015-06-16 13:58:52 +0200

    Added Highchart

--- public/js/display.js
+++ public/js/display.js
@@ -8,4 +8,37 @@
 		output.appendChild(document.createTextNode(msg + '\n'));
 	});
 
+	var request = new XMLHttpRequest();
+	request.open('GET', '/history', true);
+	request.onload = function() {
+		var data = JSON.parse(request.responseText);
+
+		var categories = [];
+		var series = [{
+			name: 'Alarms',
+			data: []
+		}];
+		data.forEach(function(d) {
+			categories.push(d.time);
+			series[0].data.push(d.alarms);
+		});
+
+		var chart = new Highcharts.Chart({
+			chart: {
+				type: 'spline',
+				renderTo: 'chart'
+			},
+			xAxis: {
+				categories: categories
+			},
+			series: series
+		});
+
+
+	};
+	request.onerror = function() {
+		// There was a connection error of some sort
+	};
+	request.send();
+
 })();
